6bbc669836b3afa3c52af3f2c77741e509a9a293
mirellanascimento <mirellagsn@gmail.com>
2024-10-02 12:36:26 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("hello world!!")
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+    "it_IT": "Ciao, Mondo!",
+}
+
+print(msg[current_language])
