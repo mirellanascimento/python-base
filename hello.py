75b5596f78abf69fc18465e2da194560d9c8bfd1
Mirella Nascimento <mirellagsn@gmail.com>
2024-10-08 11:12:57 -0300

    Update file hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,29 @@
+
 import os
 import sys
 
 print(f"{sys.argv=}")
 
-current_language = os.getenv("LANG", "en_US")[:5]
+arguments = {
+    "lang": None,
+    "count": None
+}
+for arg in sys.argv[1:]:
+    # TODO : Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid option {key}")
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language:")
 
 msg = {
     "en_US": "Hello, World!",
