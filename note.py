5a4fb674d38c4a10380f638f128004a5afd41a00
Mirella Nascimento <mirellagsn@gmail.com>
2024-10-21 12:23:23 -0300

    Alterações no arquivo note.py

--- note.py
+++ note.py
@@ -4,15 +4,16 @@ __version__ = "0.1.0"
 import sys
 import os
 
+cmds = ("read", "new") # Posso ler uma nota ou criar uma novas
 path = os.curdir # Diretorio atual
 filepath = os.path.join(path, "notes.txt") # Montar o caminho para o arquivo notes
 
 arguments = sys.argv[1:] # Argumentos para rodar no prompt 
 if not arguments:
     print("Invalid usage")
+    print(f"You must especify subcommand {cmds}")
     sys.exit(1)
 
-cmds = ("read", "new") # Posso ler uma nota ou criar uma nova
 if arguments[0] not in cmds:
     print(f"Invalid command {arguments[0]}")
 
@@ -24,17 +25,18 @@ if arguments[0] == 'read':
             print(f"Title: {title}")
             print(f"Tag: {tag}")
             print(f"Text: {text}")
+            print("-" * 30)
+            print()
 
     
 if arguments[0] == 'new':
     # Criação das notas
-    title = arguments[1], # TODO: Tratar exception
+    title = arguments[1] # TODO: Tratar exception
+    text = [
+        f'{title}',
+        input('Tag: ').strip(),
+        input('Text:\n').strip(),
+    ]    
     with open(filepath, 'a') as file_:
-        text = [
-            f'{title}',
-            input('Tag: ').strip(),
-            input('Text:\n').strip(),
-            "\n"
-        ]
-        file_.write("/t".join(text) + '\n')
+        file_.write("\t".join(text) + '\n')
 
